52567f319a74198303065458b46b659af10a8777
Matthew H. Nguyen <nguyen.huu.binh.minh@gmail.com>
2012-10-20 12:39:43 +0700

    Add rand_int function

--- random_utils.py
+++ random_utils.py
@@ -4,5 +4,10 @@ import random
 def rand_string(size=6, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for x in range(size))
 
+
+def rand_int(size=6):
+    return rand_string(size, string.digits)
+
 if __name__ == '__main__':
     print rand_string(8)
+    print rand_int()
