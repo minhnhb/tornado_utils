267ab8878a4737213c8fc776b721c219ff2c41a1
Matthew H. Nguyen <nguyen.huu.binh.minh@gmail.com>
2012-10-20 12:43:43 +0700

    Add rand_ascii function

--- random_utils.py
+++ random_utils.py
@@ -5,6 +5,9 @@ def rand_string(size=6, chars=string.ascii_uppercase + string.digits + string.as
     return ''.join(random.choice(chars) for x in range(size))
 
 
+def rand_ascii(size=6):
+    return rand_string(size, string.ascii_uppercase + string.ascii_letters)
+
 def rand_int(size=6):
     return rand_string(size, string.digits)
 
